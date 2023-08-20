2d1302fbf2c0ef4fa22f178c58cfbabf450a70f7
Muhammad Rizal Nurromdhoni <mrizaln2000@gmail.com>
2023-08-20 18:26:32 +0700

    Simplify code

--- resources/sources/cube.hpp
+++ resources/sources/cube.hpp
@@ -5,201 +5,195 @@
 #include <format>
 #include <iostream>
 
+#include <glm/glm.hpp>
+
 #include <glbinding/gl/gl.h>
 
 class Cube
 {
-    inline static constexpr std::array s_cubeVertices{
-        // clang-format off
-        -1.0f, -1.0f, -1.0f,
-         1.0f, -1.0f, -1.0f,
-         1.0f,  1.0f, -1.0f,
-         1.0f,  1.0f, -1.0f,
-        -1.0f,  1.0f, -1.0f,
-        -1.0f, -1.0f, -1.0f,
+private:
+    using Triple = glm::vec3;
+    using Pair   = glm::vec2;
 
-        -1.0f, -1.0f,  1.0f,
-         1.0f, -1.0f,  1.0f,
-         1.0f,  1.0f,  1.0f,
-         1.0f,  1.0f,  1.0f,
-        -1.0f,  1.0f,  1.0f,
-        -1.0f, -1.0f,  1.0f,
-
-        -1.0f,  1.0f,  1.0f,
-        -1.0f,  1.0f, -1.0f,
-        -1.0f, -1.0f, -1.0f,
-        -1.0f, -1.0f, -1.0f,
-        -1.0f, -1.0f,  1.0f,
-        -1.0f,  1.0f,  1.0f,
-
-         1.0f,  1.0f,  1.0f,
-         1.0f,  1.0f, -1.0f,
-         1.0f, -1.0f, -1.0f,
-         1.0f, -1.0f, -1.0f,
-         1.0f, -1.0f,  1.0f,
-         1.0f,  1.0f,  1.0f,
-
-        -1.0f, -1.0f, -1.0f,
-         1.0f, -1.0f, -1.0f,
-         1.0f, -1.0f,  1.0f,
-         1.0f, -1.0f,  1.0f,
-        -1.0f, -1.0f,  1.0f,
-        -1.0f, -1.0f, -1.0f,
-
-        -1.0f,  1.0f, -1.0f,
-         1.0f,  1.0f, -1.0f,
-         1.0f,  1.0f,  1.0f,
-         1.0f,  1.0f,  1.0f,
-        -1.0f,  1.0f,  1.0f,
-        -1.0f,  1.0f, -1.0f,
-        // clang-format on
+    struct VertexData
+    {
+        Triple m_position;
+        Triple m_normal;
+        Pair   m_texCoord;
     };
 
-    inline static constexpr std::array s_cubeNormals{
-        // clang-format off
-         0.0f,  0.0f, -1.0f,
-         0.0f,  0.0f, -1.0f,
-         0.0f,  0.0f, -1.0f,
-         0.0f,  0.0f, -1.0f,
-         0.0f,  0.0f, -1.0f,
-         0.0f,  0.0f, -1.0f,
+public:
+    static constexpr std::size_t NUM_OF_VERTICES = 36;
 
-         0.0f,  0.0f,  1.0f,
-         0.0f,  0.0f,  1.0f,
-         0.0f,  0.0f,  1.0f,
-         0.0f,  0.0f,  1.0f,
-         0.0f,  0.0f,  1.0f,
-         0.0f,  0.0f,  1.0f,
+private:
+    // clang-format off
+    inline static constexpr std::array<Triple, NUM_OF_VERTICES> s_cubeVertices{ {
+        { -1.0f, -1.0f, -1.0f },
+        {  1.0f, -1.0f, -1.0f },
+        {  1.0f,  1.0f, -1.0f },
+        {  1.0f,  1.0f, -1.0f },
+        { -1.0f,  1.0f, -1.0f },
+        { -1.0f, -1.0f, -1.0f },
 
-        -1.0f,  0.0f,  0.0f,
-        -1.0f,  0.0f,  0.0f,
-        -1.0f,  0.0f,  0.0f,
-        -1.0f,  0.0f,  0.0f,
-        -1.0f,  0.0f,  0.0f,
-        -1.0f,  0.0f,  0.0f,
+        { -1.0f, -1.0f,  1.0f },
+        {  1.0f, -1.0f,  1.0f },
+        {  1.0f,  1.0f,  1.0f },
+        {  1.0f,  1.0f,  1.0f },
+        { -1.0f,  1.0f,  1.0f },
+        { -1.0f, -1.0f,  1.0f },
 
-         1.0f,  0.0f,  0.0f,
-         1.0f,  0.0f,  0.0f,
-         1.0f,  0.0f,  0.0f,
-         1.0f,  0.0f,  0.0f,
-         1.0f,  0.0f,  0.0f,
-         1.0f,  0.0f,  0.0f,
+        { -1.0f,  1.0f,  1.0f },
+        { -1.0f,  1.0f, -1.0f },
+        { -1.0f, -1.0f, -1.0f },
+        { -1.0f, -1.0f, -1.0f },
+        { -1.0f, -1.0f,  1.0f },
+        { -1.0f,  1.0f,  1.0f },
 
-         0.0f, -1.0f,  0.0f,
-         0.0f, -1.0f,  0.0f,
-         0.0f, -1.0f,  0.0f,
-         0.0f, -1.0f,  0.0f,
-         0.0f, -1.0f,  0.0f,
-         0.0f, -1.0f,  0.0f,
+        {  1.0f,  1.0f,  1.0f },
+        {  1.0f,  1.0f, -1.0f },
+        {  1.0f, -1.0f, -1.0f },
+        {  1.0f, -1.0f, -1.0f },
+        {  1.0f, -1.0f,  1.0f },
+        {  1.0f,  1.0f,  1.0f },
 
-         0.0f,  1.0f,  0.0f,
-         0.0f,  1.0f,  0.0f,
-         0.0f,  1.0f,  0.0f,
-         0.0f,  1.0f,  0.0f,
-         0.0f,  1.0f,  0.0f,
-         0.0f,  1.0f,  0.0f
-        // clang-format on
-    };
+        { -1.0f, -1.0f, -1.0f },
+        {  1.0f, -1.0f, -1.0f },
+        {  1.0f, -1.0f,  1.0f },
+        {  1.0f, -1.0f,  1.0f },
+        { -1.0f, -1.0f,  1.0f },
+        { -1.0f, -1.0f, -1.0f },
 
-    inline static constexpr std::array s_cubeTexCoords{
-        // clang-format off
-        0.0f, 0.0f,
-        1.0f, 0.0f,
-        1.0f, 1.0f,
-        1.0f, 1.0f,
-        0.0f, 1.0f,
-        0.0f, 0.0f,
+        { -1.0f,  1.0f, -1.0f },
+        {  1.0f,  1.0f, -1.0f },
+        {  1.0f,  1.0f,  1.0f },
+        {  1.0f,  1.0f,  1.0f },
+        { -1.0f,  1.0f,  1.0f },
+        { -1.0f,  1.0f, -1.0f },
+    } };
+    // clang-format on
 
-        0.0f, 0.0f,
-        1.0f, 0.0f,
-        1.0f, 1.0f,
-        1.0f, 1.0f,
-        0.0f, 1.0f,
-        0.0f, 0.0f,
+    // clang-format off
+    inline static constexpr std::array<Triple, NUM_OF_VERTICES> s_cubeNormals{ {
+        {  0.0f,  0.0f, -1.0f },
+        {  0.0f,  0.0f, -1.0f },
+        {  0.0f,  0.0f, -1.0f },
+        {  0.0f,  0.0f, -1.0f },
+        {  0.0f,  0.0f, -1.0f },
+        {  0.0f,  0.0f, -1.0f },
 
-        1.0f, 0.0f,
-        1.0f, 1.0f,
-        0.0f, 1.0f,
-        0.0f, 1.0f,
-        0.0f, 0.0f,
-        1.0f, 0.0f,
+        {  0.0f,  0.0f,  1.0f },
+        {  0.0f,  0.0f,  1.0f },
+        {  0.0f,  0.0f,  1.0f },
+        {  0.0f,  0.0f,  1.0f },
+        {  0.0f,  0.0f,  1.0f },
+        {  0.0f,  0.0f,  1.0f },
 
-        1.0f, 0.0f,
-        1.0f, 1.0f,
-        0.0f, 1.0f,
-        0.0f, 1.0f,
-        0.0f, 0.0f,
-        1.0f, 0.0f,
+        { -1.0f,  0.0f,  0.0f },
+        { -1.0f,  0.0f,  0.0f },
+        { -1.0f,  0.0f,  0.0f },
+        { -1.0f,  0.0f,  0.0f },
+        { -1.0f,  0.0f,  0.0f },
+        { -1.0f,  0.0f,  0.0f },
 
-        0.0f, 1.0f,
-        1.0f, 1.0f,
-        1.0f, 0.0f,
-        1.0f, 0.0f,
-        0.0f, 0.0f,
-        0.0f, 1.0f,
+        {  1.0f,  0.0f,  0.0f },
+        {  1.0f,  0.0f,  0.0f },
+        {  1.0f,  0.0f,  0.0f },
+        {  1.0f,  0.0f,  0.0f },
+        {  1.0f,  0.0f,  0.0f },
+        {  1.0f,  0.0f,  0.0f },
 
-        0.0f, 1.0f,
-        1.0f, 1.0f,
-        1.0f, 0.0f,
-        1.0f, 0.0f,
-        0.0f, 0.0f,
-        0.0f, 1.0f
-        // clang-format on
-    };
+        {  0.0f, -1.0f,  0.0f },
+        {  0.0f, -1.0f,  0.0f },
+        {  0.0f, -1.0f,  0.0f },
+        {  0.0f, -1.0f,  0.0f },
+        {  0.0f, -1.0f,  0.0f },
+        {  0.0f, -1.0f,  0.0f },
 
-    float m_sideLength{};
+        {  0.0f,  1.0f,  0.0f },
+        {  0.0f,  1.0f,  0.0f },
+        {  0.0f,  1.0f,  0.0f },
+        {  0.0f,  1.0f,  0.0f },
+        {  0.0f,  1.0f,  0.0f },
+        {  0.0f,  1.0f,  0.0f }
+    } };
+    // clang-format on
 
-    // vertices data
-    std::vector<float> m_vertices;
-    std::vector<float> m_normals;
-    std::vector<float> m_texCoords;
+    // clang-format off
+    inline static constexpr std::array<Pair, NUM_OF_VERTICES> s_cubeTexCoords{ {
+        { 0.0f, 0.0f },
+        { 1.0f, 0.0f },
+        { 1.0f, 1.0f },
+        { 1.0f, 1.0f },
+        { 0.0f, 1.0f },
+        { 0.0f, 0.0f },
 
-    // interleaved vertices data
-    std::vector<float> m_interleavedVertices{};
-    int                m_interleavedVerticesStrideSize{};
+        { 0.0f, 0.0f },
+        { 1.0f, 0.0f },
+        { 1.0f, 1.0f },
+        { 1.0f, 1.0f },
+        { 0.0f, 1.0f },
+        { 0.0f, 0.0f },
 
-    // buffers
-    unsigned int m_vao;
-    unsigned int m_vbo;
+        { 1.0f, 0.0f },
+        { 1.0f, 1.0f },
+        { 0.0f, 1.0f },
+        { 0.0f, 1.0f },
+        { 0.0f, 0.0f },
+        { 1.0f, 0.0f },
+
+        { 1.0f, 0.0f },
+        { 1.0f, 1.0f },
+        { 0.0f, 1.0f },
+        { 0.0f, 1.0f },
+        { 0.0f, 0.0f },
+        { 1.0f, 0.0f },
+
+        { 0.0f, 1.0f },
+        { 1.0f, 1.0f },
+        { 1.0f, 0.0f },
+        { 1.0f, 0.0f },
+        { 0.0f, 0.0f },
+        { 0.0f, 1.0f },
+
+        { 0.0f, 1.0f },
+        { 1.0f, 1.0f },
+        { 1.0f, 0.0f },
+        { 1.0f, 0.0f },
+        { 0.0f, 0.0f },
+        { 0.0f, 1.0f }
+    } };
+    // clang-format on
+
+private:
+    float                   m_sideLength;
+    std::vector<VertexData> m_vertices;
+    unsigned int            m_vao;
+    unsigned int            m_vbo;
 
 public:
     Cube(float sideLength = 1.0f)
         : m_sideLength{ sideLength }
-        , m_vertices(s_cubeVertices.size())
-        , m_normals(s_cubeNormals.size())
-        , m_texCoords(s_cubeTexCoords.size())
-        , m_interleavedVertices(m_vertices.size() + m_normals.size() + m_texCoords.size())
-        , m_interleavedVerticesStrideSize{ 8 * sizeof(float) }
+        , m_vertices(NUM_OF_VERTICES)    // default initialize NUM_OF_VERTICES elements
     {
-        // copy vertices multiplied by sidelength
-        for (std::size_t i{ 0 }; i < std::size(m_vertices); i++) {
-            m_vertices[i] = s_cubeVertices[i] * m_sideLength / 2.0f;
+        for (std::size_t i = 0; i < NUM_OF_VERTICES; ++i) {
+            m_vertices[i].m_position = s_cubeVertices[i] * m_sideLength / 2.0f;    // divide by 2.0f since the s_cubeVertices side length is 2.0f
+            m_vertices[i].m_normal   = s_cubeNormals[i];
+            m_vertices[i].m_texCoord = s_cubeTexCoords[i];
         }
 
-        // copy normals
-        std::copy(std::begin(s_cubeNormals), std::end(s_cubeNormals), std::begin(m_normals));
-
-        // copy texCoords
-        std::copy(std::begin(s_cubeTexCoords), std::end(s_cubeTexCoords), std::begin(m_texCoords));
-
-        buildInterleavedVertices();
         setBuffers();
     }
 
+    // cube destructor must be done before glfwTerminate() is called else it will cause a segmentation fault
     ~Cube()
     {
-        // deleteBuffers();     // segmentation fault???
+        deleteBuffers();
     }
 
     void draw() const
     {
-        // bind buffer
         gl::glBindVertexArray(m_vao);
-
-        // draw
-        gl::glDrawArrays(gl::GL_TRIANGLES, 0, (gl::GLsizei)m_interleavedVertices.size());
-
-        // unbind buffer
+        gl::glDrawArrays(gl::GL_TRIANGLES, 0, static_cast<gl::GLsizei>(m_vertices.size()));
         gl::glBindVertexArray(0);
     }
 
@@ -211,32 +205,15 @@ public:
 
     void print() const
     {
-        auto& v{ m_interleavedVertices };
-        for (std::size_t i{ 0 }; i < std::size(m_interleavedVertices); i += 8) {
-            std::cout.precision(2);
-            std::cout << v[i] << '\t' << v[i + 1] << '\t' << v[i + 2] << "\t\t"
-                      << v[i + 3] << '\t' << v[i + 4] << '\t' << v[i + 5] << "\t\t"
-                      << v[i + 6] << '\t' << v[i + 7] << '\n';
+        for (const auto& vertex : m_vertices) {
+            std::cout << std::format("position: ({}, {}, {})\n", vertex.m_position.x, vertex.m_position.y, vertex.m_position.z)
+                      << std::format("normal: ({}, {}, {})\n", vertex.m_normal.x, vertex.m_normal.y, vertex.m_normal.z)
+                      << std::format("texCoord: ({}, {})\n", vertex.m_texCoord.x, vertex.m_texCoord.y)
+                      << '\n';
         }
     }
 
 private:
-    void buildInterleavedVertices()
-    {
-        for (std::size_t i{ 0 }, j{ 0 }, k{ 0 }, l{ 0 }; i < m_interleavedVertices.size(); i += 8, j += 3, k += 3, l += 2) {
-            m_interleavedVertices[i]     = m_vertices[j];
-            m_interleavedVertices[i + 1] = m_vertices[j + 1];
-            m_interleavedVertices[i + 2] = m_vertices[j + 2];
-
-            m_interleavedVertices[i + 3] = m_normals[k];
-            m_interleavedVertices[i + 4] = m_normals[k + 1];
-            m_interleavedVertices[i + 5] = m_normals[k + 2];
-
-            m_interleavedVertices[i + 6] = m_texCoords[l];
-            m_interleavedVertices[i + 7] = m_texCoords[l + 1];
-        }
-    }
-
     void setBuffers()
     {
         gl::glGenVertexArrays(1, &m_vao);
@@ -245,22 +222,17 @@ private:
         // bind
         //----
         gl::glBindVertexArray(m_vao);
-
         gl::glBindBuffer(gl::GL_ARRAY_BUFFER, m_vbo);
-        gl::glBufferData(gl::GL_ARRAY_BUFFER, static_cast<gl::GLsizei>(m_interleavedVertices.size() * sizeof(float)), m_interleavedVertices.data(), gl::GL_STATIC_DRAW);
+        gl::glBufferData(gl::GL_ARRAY_BUFFER, static_cast<gl::GLsizei>(sizeof(VertexData) * m_vertices.size()), &m_vertices.front(), gl::GL_STATIC_DRAW);
 
         // vertex attribute
         //-----------------
-        // position
-        gl::glVertexAttribPointer(0, 3, gl::GL_FLOAT, gl::GL_FALSE, m_interleavedVerticesStrideSize, (void*)(0));
+        auto stride{ static_cast<gl::GLsizei>(sizeof(VertexData)) };
+        gl::glVertexAttribPointer(0, decltype(VertexData::m_position)::length(), gl::GL_FLOAT, gl::GL_FALSE, stride, (void*)(offsetof(VertexData, m_position)));
+        gl::glVertexAttribPointer(1, decltype(VertexData::m_normal)::length(), gl::GL_FLOAT, gl::GL_FALSE, stride, (void*)(offsetof(VertexData, m_normal)));
+        gl::glVertexAttribPointer(2, decltype(VertexData::m_texCoord)::length(), gl::GL_FLOAT, gl::GL_FALSE, stride, (void*)(offsetof(VertexData, m_texCoord)));
         gl::glEnableVertexAttribArray(0);
-
-        // normal
-        gl::glVertexAttribPointer(1, 3, gl::GL_FLOAT, gl::GL_FALSE, m_interleavedVerticesStrideSize, (void*)(3 * sizeof(float)));
         gl::glEnableVertexAttribArray(1);
-
-        // texcoords
-        gl::glVertexAttribPointer(2, 2, gl::GL_FLOAT, gl::GL_FALSE, m_interleavedVerticesStrideSize, (void*)(6 * sizeof(float)));
         gl::glEnableVertexAttribArray(2);
 
         // unbind
